4c490fb82da551287b9ac701eaecbd1143a1581a
JimLynchCodes <mrdotjim@gmail.com>
2022-02-06 23:09:13 -0500

    gucci

--- angular-v1/src/app/pages/trade-bot-page/trade-bot-page.component.ts
+++ angular-v1/src/app/pages/trade-bot-page/trade-bot-page.component.ts
@@ -1,101 +1,8 @@
 
 import { Component, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { TdApiService, BuyOrSell } from '../../services/td-api/td-api.service';
-import { ToastManagerService } from '../../services/toast-manager/toast-manager.service';
-import { BsModalService } from 'ngx-bootstrap/modal';
-import { EnableGainslockerModalService } from '../../components/modals/enable-gainslock-confirm/enable-gainslock-modal.service';
-import { AskCancelOrderModalService } from '../../components/modals/ask-cancel-order/ask-cancel-order-modal.service';
-import { AskPlaceTradeModalService } from 'src/app/components/modals/ask-place-trade/ask-place-trade-modal.service';
-import { interval } from 'rxjs';
-import { decideLimitPrice } from 'src/app/services/limit-price-decider';
-import { decideBuyOrSell } from 'src/app/services/buy-or-sell-decider';
+import { TdApiService } from '../../services/td-api/td-api.service';
 import { StrangulatorService } from 'src/app/strangulator.service';
-
-const fakeBuyOrder1 = {
-  quantity: 1,
-  price: 2,
-  orderLegCollection: [{
-    instruction: 'BUY',
-    instrument: {
-      symbol: 'MSFT'
-    }
-  }],
-  reasons: [{
-    text: 'spike up in volume',
-  },
-  {
-    text: 'just picked up a great new CTO'
-  },
-  {
-    text: 'price has been steadily climbing'
-  },
-  {
-    text: 'triple gainers list 10/14/2020 with 80% buy recc'
-  }]
-
-}
-
-const fakeBuyOrder2 = {
-  quantity: 2,
-  price: 3.45,
-  orderLegCollection: [{
-    instruction: 'BUY',
-    instrument: {
-      symbol: 'AAPL'
-    }
-  }],
-  reasons: [{
-    text: 'spike up in volume',
-  },
-  {
-    text: 'just picked up a great new CTO'
-  },
-  {
-    text: 'price has been steadily climbing'
-  },
-  {
-    text: 'triple gainers list 10/14/2020 with 80% buy recc'
-  }
-  ]
-
-}
-
-const fakeBuyOrder3 = {
-  quantity: 1,
-  price: 265.23,
-  orderLegCollection: [{
-    instruction: 'BUY',
-    instrument: {
-      symbol: 'TSLA'
-    }
-  }],
-  reasons: [{
-    text: 'spike up in volume',
-  },
-  {
-    text: 'just picked up a great new CTO'
-  },
-  {
-    text: 'price has been steadily climbing'
-  },
-  {
-    text: 'triple gainers list 10/14/2020 with 80% buy recc'
-  }
-  ]
-
-}
-
-const fakeSellOrder1 = {
-  quantity: 1,
-  price: 1000,
-  orderLegCollection: [{
-    instruction: 'SELL',
-    instrument: {
-      symbol: 'TSLA'
-    }
-  }]
-}
 
 @Component({
   selector: 'app-trade-bot-page',
@@ -108,28 +15,15 @@ export class TradeBotPageComponent {
 
   accountsData: any;
 
-  // largecapTickers = ['TSM', 'TSLA', 'BABA', 'WMT', 'DIS', 'BAC', 'NVDA', 'PYPL', 'INTC', 'NFLX']
-  largecapTickers = []
-  // etfTickers = ['IWM', 'QQQ', 'EEM', 'EWZ', 'IWM', 'XLF', 'SQQQ', 'SLV', 'GDX', 'XLE']
-  // etfTickers = ['SPY', 'QQQ', 'SOXL']
-  etfTickers = []
-  // memeStonkTickers = ['GME', 'AMC', 'MVIS', 'VIAC', 'RKT', 'AMD', 'MSFT', 'PLTR', 'TLRY', 'NIO', 'UBER', 'APHA', 'EBAY', 'MDB']
-  // solidProfitMakersTickers = ['MELI', 'APPL', 'AMZN', 'FB', 'COST', 'EW', 'BAND', 'SPOT', 'MAXR', 'LW', 'HD', 'MARA', 'BITO']
-  solidProfitMakersTickers = []
-  // memeStonkTickers = ['GME', 'AMC', 'MVIS', 'VIAC', 'RKT', 'AMD', 'MSFT', 'PLTR', 'TLRY', 'NIO', 'UBER', 'APHA', 'EBAY', 'MDB', 'NFLX', 'TSLA', 'NVDA', 'DIS', 'SNAP', 'COST', 'SBUX', 'UNH', 'CVS', 'UPS', 'SHW']
-  // memeStonkTickers = ['GME', 'MDB']
-  // memeStonkTickers = ['COF', 'SNAP']
-  // memeStonkTickers = ['AMZN', 'GOOGL', 'TSLA', 'LYFT', 'SAVE']
-  memeStonkTickers = ['AMZN', 'GOOGL', 'TSLA', 'HOG', 'PFE', 'PTON', 'TWLO', 'APPS', 'CRSR', 'CHGG', 'SPX', 'IWM', 'SMH', 'DDOG', 'NET', 'MGM', 'GLBE', 'CMG', 'SEB', 'BKNG']
-  // bestInClassTickers = ['GOOG', 'GOOGL', 'APPL', 'AMZN', 'FB', 'COST']
-  bestInClassTickers = []
+  largecapTickers = ['GOOG', 'GOOGL', 'TSM', 'TSLA', 'BABA', 'WMT', 'DIS', 'BAC', 'NVDA', 'AMZN', 'SEB', 'BKNG', 'MKL', 'CMG']
+  // etfTickers = ['IWM', 'QQQ', 'EEM', 'EWZ', 'IWM', 'XLF', 'SQQQ', 'SLV', 'GDX', 'XLE', 'TQQQ', 'SOXL', 'SMH', 'SPY', 'SPX']
+  etfTickers = ['SPX']
+  growthStockTickers = ['HOG', 'PFE', 'PTON', 'TWLO', 'APPS', 'CRSR', 'CHGG', 'DDOG', 'NET', 'MGM', 'GLBE']
 
   rowsInTickerTable = 0
   arrayOfRowIndicies = []
 
   allSymbols = []
-
-  // strangulations = [];
   analyzedSpreads = [];
   analyzedDebitSpreads = [];
 
@@ -144,108 +38,71 @@ export class TradeBotPageComponent {
 
   async ngOnInit() {
 
-    setTimeout(() => {
+    // setTimeout(() => {
 
-      console.log('ok....')
+    //   this.analyzedDebitSpreads = this.analyzedDebitSpreads.sort((a, b) => {
+    //     return a.totalPremiumSum > b.totalPremiumSum ? -1 : 1
+    //   })
 
-      this.analyzedDebitSpreads = this.analyzedDebitSpreads.sort((a, b) => {
-        console.log('#$# OR' + a.totalPremiumSum)
+    //   console.log('#$# sorted ', this.analyzedDebitSpreads)
 
-        return a.totalPremiumSum > b.totalPremiumSum ? -1 : 1
-      })
-
-      console.log('#$# sorted ', this.analyzedDebitSpreads)
-
-      console.log('#$# pushed ', this.analyzedDebitSpreads)
-    }, 20_000)
+    // }, 20_000)
 
     this.allSymbols = [
       ...this.largecapTickers,
       ...this.etfTickers,
-      ...this.memeStonkTickers,
-      ...this.solidProfitMakersTickers
+      ...this.growthStockTickers
     ]
 
     this.rowsInTickerTable = Math.max(
       this.largecapTickers.length,
       this.etfTickers.length,
-      this.memeStonkTickers.length,
-      this.solidProfitMakersTickers.length
+      this.growthStockTickers.length
     );
 
     await this.tdApiSvc.init();
 
     this.arrayOfRowIndicies = Array.from(Array(this.rowsInTickerTable).keys())
 
-    if (!this.accountsData) {
+    console.log('#$# looping over tickers: ', this.allSymbols);
 
-      // this.accountsData = await this.tdApiSvc.getPositions();
+    await this.recalcSpreads();
 
-      console.log('#$# looping over tickers: ', this.allSymbols)
+  }
 
-      this.analyzedDebitSpreads = []
+  private async recalcSpreads() {
+    this.analyzedDebitSpreads = []
 
-      this.allSymbols.forEach(async symbol => {
+    this.allSymbols.forEach(async symbol => {
 
-        console.log('#$# calling for chain: ', symbol);
+      let optionChain: any
 
-        let optionChain: any
+      try {
+        optionChain = await this.tdApiSvc.getOptionChainForSymbol(symbol);
+      }
+      catch (err) {
+        console.log('#$# couldn\'t get options chains for: ', symbol)
+      }
 
-        try {
-          optionChain = await this.tdApiSvc.getOptionChainForSymbol(symbol);
-        }
-        catch (err) {
-          console.log('#$# couldn\'t get options chains for: ', symbol)
-        }
+      if (optionChain['underlying']) {
 
-        // const minAcceptableDelta = -0.002
-        // const maxAcceptableDelta = 0.002
+        console.log('#$# analyzing spreads for ', symbol, optionChain.putExpDateMap, optionChain.underlying)
 
-        // const minAcceptableGamma = -0.01
-        // const maxAcceptableGamma = 0.01
+        const callSpreads = this.strangulator.getPennyCallSpreads(symbol, optionChain.callExpDateMap, optionChain.underlying)
+        const putSpreads = this.strangulator.getPennyPutSpreads(symbol, optionChain.putExpDateMap, optionChain.underlying)
 
-        // console.log('chain for ' + symbol + ': ', optionChain)
+        this.analyzedDebitSpreads.push(...callSpreads)
+        this.analyzedDebitSpreads.push(...putSpreads)
 
-        if (optionChain['underlying']) {
+        this.sortDebitSpreads();
 
-          console.log('#$# analyzing spreads for ', symbol, optionChain.putExpDateMap, optionChain.underlying)
+      }
+    })
+  }
 
-          const spreadsForTicker = this.strangulator.analyzePutSpreads(symbol, optionChain.putExpDateMap, optionChain.underlying)
-
-          const callSpreads = this.strangulator.getPennyCallSpreads(symbol, optionChain.callExpDateMap, optionChain.underlying)
-          const putSpreads = this.strangulator.getPennyPutSpreads(symbol, optionChain.putExpDateMap, optionChain.underlying)
-
-          console.log('#$# call spreads ------')
-          console.log('#$#', callSpreads)
-
-          // const strangulation = this.strangulator.strangulate(
-          //   optionChain['callExpDateMap'],
-          //   optionChain['putExpDateMap'],
-          //   optionChain['underlying']['last'],
-          //   minAcceptableDelta,
-          //   maxAcceptableDelta,
-          //   minAcceptableGamma,
-          //   maxAcceptableGamma
-          // )
-
-          this.analyzedDebitSpreads.push(...callSpreads)
-          this.analyzedDebitSpreads.push(...putSpreads)
-
-          // this.analyzedSpreads.push(spreadsForTicker);
-
-
-          // this.strangulations = this.strangulations.filter(strangulation => {
-          //   return strangulation.length > 0
-          // })
-
-          // this.strangulations = this.strangulations.sort((a, b) => {
-          //   return a[0].thetaPower > b[0].thetaPower ? 1 : -1
-          // })
-
-          // console.log('the gud ones are..... ', this.strangulations);
-
-        }
-      })
-    }
+  private sortDebitSpreads() {
+    this.analyzedDebitSpreads = this.analyzedDebitSpreads.sort((a, b) => {
+      return a.totalPremiumSum > b.totalPremiumSum ? -1 : 1
+    })
   }
 }
--- angular-v1/src/app/strangulator.service.ts
+++ angular-v1/src/app/strangulator.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
 
+const MAXIMUM_DEBIT_PRICE = 0.00;
+
+const MINIMUM_ACCEPTABLE_BID = 1.00;
+
 @Injectable({
   providedIn: 'root'
 })
@@ -7,14 +11,10 @@ export class StrangulatorService {
 
   getPennyCallSpreads(symbol: any, callExpDateMap: any, underlyingData: any) {
 
-    console.log('getting penny call spreads for: ', symbol, callExpDateMap);
-
     const gudOnes = [];
 
     // loop through expiration tables
     Object.entries(callExpDateMap).forEach(([expirationString, calls]) => {
-
-      console.log('now looking at: ' + symbol + 'expirationString: ', expirationString);
 
       // loop through strikes for a given expiration
       const strikes = Object.keys(calls)
@@ -23,38 +23,28 @@ export class StrangulatorService {
 
         const strike = strikes[i];
 
-        // console.log(ticker + ' strike: ', strike);
-        // console.log(ticker + ' put: ', puts[strike]);
-
         if (strikes[i + 1]) {
 
           const lowerStrike = calls[strikes[i]][0].strikePrice;
           const higherStrike = calls[strikes[i + 1]][0].strikePrice;
 
-          console.log('#$# comparing strike ' + lowerStrike + ' to next higher: ' + higherStrike)
-
           const lowerStrikeBid = calls[strikes[i]][0].bid
           const lowerStrikeAsk = calls[strikes[i]][0].ask;
           const lowerStrikeMidpoint = (lowerStrikeAsk + lowerStrikeBid) / 2;
-
-          console.log('#$# (strike ' + calls[strikes[i]][0].strikePrice + '), bid: ' + lowerStrikeBid + ', ask: ' + lowerStrikeAsk, ', mid: ', lowerStrikeMidpoint)
 
           const higherStrikeBid = calls[strikes[i + 1]][0].bid
           const higherStrikeAsk = calls[strikes[i + 1]][0].ask;
           const higherStrikeMidpoint = (higherStrikeAsk + higherStrikeBid) / 2;
 
-          console.log('#$# (strike ' + calls[strikes[i + 1]][0].strikePrice + '), bid: ' + higherStrikeBid + ', ask: ' + higherStrikeAsk, ', mid: ', higherStrikeMidpoint)
-
           const midpointDifference = lowerStrikeMidpoint - higherStrikeMidpoint;
 
-          console.log('#$# debit difference: ', midpointDifference);
+          const expirationDate = expirationString.substr(0, +expirationString.indexOf(':'))
+          const expirationDaysLeft = expirationString.substr(expirationString.indexOf(':') + 1)
 
-          // if (midpointDifference < 0.05 && midpointDifference > -0.05) {
-          if (midpointDifference < 0.05) {
-            console.log('#$# WOAHHHHH!!! ', midpointDifference);
+          if (midpointDifference < MAXIMUM_DEBIT_PRICE) {
 
-            if (higherStrikeBid < 0.05 || lowerStrikeBid < 0.05) {
-              console.log('#$# Ahhhh, false alarm...')
+            if (higherStrikeBid < MINIMUM_ACCEPTABLE_BID || lowerStrikeBid < MINIMUM_ACCEPTABLE_BID) {
+              console.log('Ahhhh, false alarm...')
             }
             else {
 
@@ -67,11 +57,10 @@ export class StrangulatorService {
                 underlyingPrice: underlyingData.last,
                 higherStrikeMidpoint,
                 lowerStrikeMidpoint,
-                expirationCycle: expirationString,
+                expirationDate: expirationDate,
+                expirationDaysLeft: expirationDaysLeft,
                 midpointDifference,
               }
-
-              console.log('#$# Gucci? ', gudOne)
 
               gudOnes.push(gudOne)
             }
@@ -79,7 +68,7 @@ export class StrangulatorService {
 
         }
         else {
-          console.log('#$# on last strike (no next higher one): ', strikes[i]);
+          // OK
         }
 
       }
@@ -88,25 +77,16 @@ export class StrangulatorService {
 
     const gudOnesSorted = gudOnes.sort((a, b) => a.totalPremiumSum > b.totalPremiumSum ? -1 : 1)
 
-    console.log(gudOnesSorted.length)
-    console.log('done calculating spreads!')
-
-    // return gudOnesSorted.slice(0, 25);
     return gudOnesSorted;
-
   }
 
 
   getPennyPutSpreads(symbol: any, putExpDateMap: any, underlyingData: any) {
 
-    console.log('getting penny put spreads for: ', symbol, putExpDateMap);
-
     const gudOnes = [];
 
     // loop through expiration tables
     Object.entries(putExpDateMap).forEach(([expirationString, puts]) => {
-
-      console.log('now looking at: ' + symbol + 'expirationString: ', expirationString);
 
       // loop through strikes for a given expiration
       const strikes = Object.keys(puts)
@@ -115,37 +95,27 @@ export class StrangulatorService {
 
         const strike = strikes[i];
 
-        // console.log(ticker + ' strike: ', strike);
-        // console.log(ticker + ' put: ', puts[strike]);
-
         if (strikes[i + 1]) {
 
           const lowerStrike = puts[strikes[i]][0].strikePrice;
           const higherStrike = puts[strikes[i + 1]][0].strikePrice;
 
-          console.log('#$# comparing strike ' + lowerStrike + ' to next higher: ' + higherStrike)
-
           const lowerStrikeBid = puts[strikes[i]][0].bid
           const lowerStrikeAsk = puts[strikes[i]][0].ask;
           const lowerStrikeMidpoint = (lowerStrikeAsk + lowerStrikeBid) / 2;
-
-          console.log('#$# (strike ' + puts[strikes[i]][0].strikePrice + '), bid: ' + lowerStrikeBid + ', ask: ' + lowerStrikeAsk, ', mid: ', lowerStrikeMidpoint)
 
           const higherStrikeBid = puts[strikes[i + 1]][0].bid
           const higherStrikeAsk = puts[strikes[i + 1]][0].ask;
           const higherStrikeMidpoint = (higherStrikeAsk + higherStrikeBid) / 2;
 
-          console.log('#$# (strike ' + puts[strikes[i + 1]][0].strikePrice + '), bid: ' + higherStrikeBid + ', ask: ' + higherStrikeAsk, ', mid: ', higherStrikeMidpoint)
-
           const midpointDifference = higherStrikeMidpoint - lowerStrikeMidpoint;
 
-          console.log('#$# debit difference: ', midpointDifference);
+          const expirationDate = expirationString.substr(0, +expirationString.indexOf(':'))
+          const expirationDaysLeft = expirationString.substr(expirationString.indexOf(':') + 1)
 
-          // if (midpointDifference < 0.05 && midpointDifference > -0.05) {
-          if (midpointDifference < 0.05) {
-            console.log('#$# WOAHHHHH!!! ', midpointDifference);
+          if (midpointDifference < MAXIMUM_DEBIT_PRICE) {
 
-            if (higherStrikeBid < 0.05 || lowerStrikeBid < 0.05) {
+            if (higherStrikeBid < MINIMUM_ACCEPTABLE_BID || lowerStrikeBid < MINIMUM_ACCEPTABLE_BID) {
               console.log('#$# Ahhhh, false alarm...')
             }
             else {
@@ -159,11 +129,10 @@ export class StrangulatorService {
                 underlyingPrice: underlyingData.last,
                 higherStrikeMidpoint,
                 lowerStrikeMidpoint,
-                expirationCycle: expirationString,
+                expirationDate: expirationDate,
+                expirationDaysLeft: expirationDaysLeft,
                 midpointDifference,
               }
-
-              console.log('#$# Gucci? ', gudOne)
 
               gudOnes.push(gudOne)
             }
@@ -180,182 +149,8 @@ export class StrangulatorService {
 
     const gudOnesSorted = gudOnes.sort((a, b) => a.totalPremiumSum > b.totalPremiumSum ? -1 : 1)
 
-    console.log(gudOnesSorted.length)
-    console.log('done calculating spreads!')
-
-    // return gudOnesSorted.slice(0, 25);
     return gudOnesSorted;
 
-  }
-
-  constructor() { }
-
-  analyzePutSpreads(ticker: string, putExpirationMap: any, underlyingData: any) {
-
-    console.log('undy', underlyingData)
-    console.log('undy last', underlyingData.last)
-    console.table('undy', underlyingData)
-
-    console.log('analyzing spreads for: ', ticker, putExpirationMap);
-
-    const gudOnes = [];
-
-    // loop through expiration tables
-    Object.entries(putExpirationMap).forEach(([expirationString, puts]) => {
-
-      console.log(ticker + 'expirationString: ', expirationString);
-      console.log(ticker + ' puts: ', puts);
-
-      // loop through strikes for a given expiration cycle
-      // Object.entries(puts).forEach( ([strike, put], entryIndex) => {
-
-      //   console.log(ticker + 'strike: ', strike);
-      //   console.log(ticker + ' put: ', put);
-
-      //   if (puts)
-
-      // })
-
-      const strikes = Object.keys(puts)
-
-      for (var i = 0; i < strikes.length; i++) {
-
-        const strike = strikes[i];
-
-        // console.log(ticker + ' strike: ', strike);
-        console.log(ticker + ' put: ', puts[strike]);
-        // console.log(ticker + ' next put: ', strikes[i + 1])
-
-        if (strikes[i + 1]) {
-
-          const lowerStrike = puts[strikes[i]][0].strikePrice;
-          // long put (lower strike)
-
-          // console.log(ticker + ' -- lower strike: ' + lowerStrike)
-          // console.log(ticker + ' -- lower strike theta: ' + puts[strikes[i]][0].theta)
-
-          const lowerStrikePutBid = puts[strikes[i]][0].bid
-
-          const lowerStrikePutBidAskMidpoint = (puts[strikes[i]][0].ask + lowerStrikePutBid) / 2
-
-          // console.log(ticker + ' -- higher strike bid: ' + puts[strikes[i]][0].bid)
-          // console.log(ticker + ' -- higher strike ask' + puts[strikes[i]][0].ask)
-          // console.log(ticker + ' -- higher strike bid/ask midpoint: ' + higherStrikePutBidAskMidpoint)
-
-          const higherStrike = puts[strikes[i + 1]][0].strikePrice;
-          const higherStrikePutBid = puts[strikes[i + 1]][0].bid;
-          // short put (higher strike)
-
-          // console.log(ticker + ' -- higher strike: ' + higherStrike)
-          // console.log(ticker + ' -- higher strike theta: ' + puts[strikes[i + 1]][0].theta)
-
-          const higherStrikePutBidAskMidpoint = (puts[strikes[i + 1]][0].ask + higherStrikePutBid) / 2
-
-          // console.log(ticker + ' -- lower strike bid: ' + puts[strikes[i + 1]][0].bid)
-          // console.log(ticker + ' -- lower strike ask' + puts[strikes[i + 1]][0].ask)
-          // console.log(ticker + ' -- lower strike bid/ask midpoint: ' + lowerStrikePutBidAskMidpoint)
-
-          const netTheta = (puts[strikes[i]][0].theta - puts[strikes[i + 1]][0].theta);
-          // console.log(ticker + ' ----- net theta: ' + netTheta);
-
-          const netDelta = (puts[strikes[i]][0].delta - puts[strikes[i + 1]][0].delta);
-          // console.log(ticker + ' ----- net delta: ' + netDelta);
-
-          const netCreditForSpread = higherStrikePutBidAskMidpoint - lowerStrikePutBidAskMidpoint
-
-          // console.log(ticker + ' ----- net credit: ' + netCreditForSpread);
-
-          const strikeWidth = puts[strikes[i + 1]][0].strikePrice - puts[strikes[i]][0].strikePrice
-
-          // console.log(ticker + ' ----- strikeWidth: ' + strikeWidth);
-
-          const maxLoss = strikeWidth - netCreditForSpread
-
-          // console.log(ticker + ' ----- max loss for spread: ' + maxLoss);
-
-          const tomlScore = netTheta / maxLoss;
-
-          // console.log(ticker + ' ----- toml score: ' + tomlScore);
-
-          // only positive theta, no debit spreads
-          if (netTheta > 0 && netCreditForSpread > 0 && tomlScore < Infinity) {
-
-            // don't go for options with zero bidders!
-            if (lowerStrikePutBid > 0 && higherStrikePutBid > 0) {
-
-              // only OTM puts...
-              if (higherStrike < underlyingData.last) {
-
-                const fortnightAway = new Date(Date.now() + 1.21e9);
-
-                const expirationDate = new Date(puts[strike][0].expirationDate);
-
-
-                console.log('time exp: ', puts[strike][0].expirationDate);
-                console.log('time 2: ', fortnightAway);
-                console.log('time: ', expirationDate);
-
-                if (expirationDate > fortnightAway) {
-
-                  gudOnes.push({
-                    ticker,
-                    spreadType: 'CALLS',
-                    underlyingPrice: underlyingData.last,
-                    higherStrike,
-                    lowerStrike,
-                    higherStrikePutBidAskMidpoint,
-                    lowerStrikePutBidAskMidpoint,
-                    expirationCycle: expirationString,
-                    netTheta,
-                    netDelta,
-                    netCreditForSpread,
-                    maxLoss,
-                    tomlScore
-                  })
-                }
-              }
-            }
-          }
-        }
-      }
-    })
-
-    const gudOnesSorted = gudOnes.sort((a, b) => a.tomlScore > b.tomlScore ? -1 : 1)
-
-    console.log(gudOnesSorted.length)
-    console.log('done calculating spreads!')
-
-    return gudOnesSorted.slice(0, 25);
-
-  }
-
-  private hasNanGreeks(currentCallOrPut) {
-
-    // console.log('checking option\'s greeks: ', currentCallOrPut)
-
-    if (
-      !currentCallOrPut.theta ||
-      !currentCallOrPut.delta ||
-      !currentCallOrPut.gamma ||
-      currentCallOrPut.delta == 'N/A' ||
-      currentCallOrPut.delta == 'n/a' ||
-      currentCallOrPut.delta == 'Nan' ||
-      currentCallOrPut.delta == 'NaN' ||
-      currentCallOrPut.gamma == 'N/A' ||
-      currentCallOrPut.gamma == 'n/a' ||
-      currentCallOrPut.gamma == 'Nan' ||
-      currentCallOrPut.gamma == 'NaN' ||
-      currentCallOrPut.theta == 'N/A' ||
-      currentCallOrPut.theta == 'n/a' ||
-      currentCallOrPut.theta == 'Nan' ||
-      currentCallOrPut.theta == 'NaN'
-    ) {
-
-      // console.log('the greeks are NaN!')
-      return true;
-    }
-
-    return false;
   }
 
 }
